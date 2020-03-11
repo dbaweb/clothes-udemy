4d8cdc46ee93539c1def87231bb26d01b459cae8
Arnaud De Boeck <deboeck.arnaud@gmail.com>
2020-03-11 14:50:58 +0100

    Sign up & users in firestore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./components/pages/homepage/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndOutPage from "./components/pages/sign-in-and-out/sign-in-and-out.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -21,8 +21,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = createUserProfileDocument(userAuth);
+
+        (await userRef).onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/components/pages/sign-in-and-out/sign-in-and-out.component.jsx
+++ src/components/pages/sign-in-and-out/sign-in-and-out.component.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 import "./sign-in-and-out.styles.scss";
 import SignIn from "../../sign-in/sign-in.component";
+import SignUp from "../../sign-up/sign-up.component";
 
-const SignInAndOutPage = () => <SignIn></SignIn>;
+const SignInAndOutPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn></SignIn>
+    <SignUp></SignUp>
+  </div>
+);
 
 export default SignInAndOutPage;
